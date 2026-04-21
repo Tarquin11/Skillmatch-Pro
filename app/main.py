76f53addfdf3f7c60d4ac7a4a60866f875e6be11
Tarquin11 <zarrouk.moataz2003@gmail.com>
2026-04-22 00:06:23 +0100

    ajouter certificat HTTPS

--- app/main.py
+++ app/main.py
@@ -38,8 +38,8 @@ app = FastAPI(title="SkillMatch Pro", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4200",
-        "http://127.0.0.1:4200",
+        "https://localhost:4200",
+        "https://127.0.0.1:4200",
     ],
     allow_credentials=True,
     allow_methods=["*"],
